88adea9a9a150e7ca2590aec659811872f99e717
fyanael <yanael.fillon@ynov.com>
2026-03-16 16:34:21 +0100

    chill

--- api/api.js
+++ api/api.js
@@ -1,8 +1,5 @@
 require('dotenv').config(); // Charge les variables d'environnement
 
-const api_key = process.env.API_KEY;
-
-//fetch('https://api.themoviedb.org/3/authentication', options)
 fetch(`https://api.themoviedb.org/3/movie/550?api_key=${process.env.TMDB_TOKEN}`)
   .then(res => res.json())
   .then(res => console.log(res))
