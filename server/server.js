5f09d87a47dcbdccff962d1698c4b34c7b33a1d7
fyanael <yanael.fillon@ynov.com>
2026-04-10 16:59:20 +0200

    feat carroussel

--- server/server.js
+++ server/server.js
@@ -26,8 +26,9 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 app.get('/api/movies', async (req, res) => {
     const page = req.query.page || 1;
+    const category = req.query.category || 'popular';
     const language = 'fr-FR';
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_TOKEN}&language=${language}&page=${page}`;
+    const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${process.env.TMDB_TOKEN}&language=${language}&page=${page}`;
 
     try {
         const response = await fetch(url);
@@ -59,6 +60,10 @@ app.get('/allmovies', (req, res) => {
 app.get('/account_creation', (req, res) => {
     // route to allmovies.html for the button "browse"
     res.sendFile(path.join(__dirname, '../client/templates/account_creation.html'));
+});
+
+app.get('/carroussel', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/templates/carroussel.html'));
 });
 
 app.get('/index', (req, res) => {
